9d6bbe7f316403f774441c1949f9d921678beeb0
PatchedDev <danila.kardashevskii@icloud.com>
2023-06-22 20:22:47 +0200

    Initial Commit

---
+++ CryptoMetricsSwiftUI/CryptoMetricsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMetricsSwiftUIApp.swift
+//  CryptoMetricsSwiftUI
+//
+//  Created by Danila Kardashevkii on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMetricsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
